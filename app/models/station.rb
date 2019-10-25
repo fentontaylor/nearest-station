2fdf5978e70f6cbfe6181885c7eaf4853bc09ecb
Fenton Taylor <fentontaylor@gmail.com>
2019-10-25 10:17:37 -0600

    Refactor station address

--- app/models/station.rb
+++ app/models/station.rb
@@ -6,14 +6,20 @@ class Station
               :latitude,
               :longitude,
               :name
-              
+
   def initialize(hash)
     @access_times = hash[:access_days_time]
-    @address = hash[:street_address]
+    @address = build_address(hash)
     @distance = hash[:distance]
     @fuel_types = hash[:fuel_type_code]
     @latitude = hash[:latitude]
     @longitude = hash[:longitude]
     @name = hash[:station_name]
+  end
+
+  private
+
+  def build_address(hash)
+    "#{hash[:street_address]}, #{hash[:city]}, #{hash[:state]} #{hash[:zip]}"
   end
 end
