2a5e2937155435d5954afb7b7c3faeccc56bff65
Fenton Taylor <fentontaylor@gmail.com>
2019-10-25 11:01:19 -0600

    Got plain text directions from google routes api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,29 @@
 class SearchController < ApplicationController
   def index
-    location = params['location']
+    origin = params['location']
     root_url = 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest'
+
     response = Faraday.get(root_url) do |req|
       req.params['api_key'] = ENV['NREL_KEY']
-      req.params['location'] = location
+      req.params['location'] = origin
       req.params['fuel_type'] = 'ELEC'
       req.params['format'] = 'json'
     end
 
     data = JSON.parse(response.body, symbolize_names: true)
     @station = Station.new(data[:fuel_stations].first)
+
+    google_url = 'https://maps.googleapis.com/maps/api/directions/json'
+
+    g_response = Faraday.get(google_url) do |req|
+      req.params['origin'] = origin
+      req.params['destination'] = @station.address
+      req.params['key'] = ENV['GOOGLE_KEY']
+    end
+
+    g_data = JSON.parse(g_response.body, symbolize_names: true)
+    @directions = g_data[:routes].first[:legs].first[:steps].map do |step|
+      ActionController::Base.helpers.strip_tags(step[:html_instructions])
+    end
   end
 end
--- spec/features/user_searches_for_nearest_station_spec.rb
+++ spec/features/user_searches_for_nearest_station_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 describe 'As a user' do
   context 'on the welcome page, I select a starting location and click "Find Nearest Station"' do
     it 'takes me to /search where I see station info and directions' do
-      # As a user
-      # When I visit "/"
       visit '/'
-      # And I select "Turing" form the start location drop down (Note: Use the existing search form)
+
       select 'Turing', from: 'location'
-      # And I click "Find Nearest Station"
+
       click_button 'Find Nearest Station'
-      # Then I should be on page "/search"
+
       expect(current_path).to eq('/search')
-      # Then I should see the closest electric fuel station to me.
+
       within('.station-info') do
         expect(page).to have_css('.station-name')
         expect(page).to have_content('Station name: Seventeenth Street Plaza')
@@ -29,18 +27,16 @@ describe 'As a user' do
         expect(page).to have_css('.station-access-times')
         expect(page).to have_content("Access times: MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified")
       end
-      # For that station I should see
-      # - Name
-      # - Address
-      # - Fuel Type
-      # - Distance
-      # - Access Times
-      #
+
       # I should also see:
       # - the distance of the nearest station
       # - the travel time from Turing to that fuel station (should be 0.1 miles)
       # - The HTML direction instructions to get to that fuel station
       #   "Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>"
+      within '.directions' do
+        expect(page).to have_content('Head southeast on 17th St toward Larimer St')
+        expect(page).to have_content('Turn left onto Lawrence St Destination will be on the left')
+      end
     end
   end
 end
