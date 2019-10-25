8d4a55fca80864b824370a49f52edc7a9cf8484d
Fenton Taylor <fentontaylor@gmail.com>
2019-10-25 09:53:49 -0600

    API returning list of stations, start Station PORO

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,16 @@
 class SearchController < ApplicationController
   def index
+    location = params['location']
+    root_url = 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest'
+    response = Faraday.get(root_url) do |req|
+      req.params['api_key'] = ENV['NREL_KEY']
+      req.params['location'] = location
+      req.params['fuel_type'] = 'ELEC'
+      req.params['format'] = 'json'
+    end
 
+    data = JSON.parse(response.body, symbolize_names: true)
+    @station = data[:fuel_stations].first
+    binding.pry
   end
 end
---
+++ spec/models/station_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Station do
+  it "can initialize with a hash" do
+    
+  end
+end
